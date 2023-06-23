b50c32a3f75780876732e05cdbb09077338c7b98
Andriy Konoval <ceperiv@gmail.com>
2023-06-23 14:56:34 +0300

    removed test button

--- src/app/pages/cats/cats.ts
+++ src/app/pages/cats/cats.ts
@@ -7,9 +7,4 @@ import {Component} from '@angular/core';
 })
 export class Cats {
 
-  xxx() {
-    console.log(document.documentElement.scrollTop)
-    console.log(document?.scrollingElement?.scrollTop)
-    console.log(window.pageYOffset)
-  }
 }
